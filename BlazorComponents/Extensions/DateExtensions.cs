7d826280e42afee8c096d8ef848d9336553d8088
antony lucisano <antony.lucisano@ukfast.co.uk>
2019-11-05 13:08:15 +0000

    Extensions updated to properly validate date

--- BlazorComponents/Extensions/DateExtensions.cs
+++ BlazorComponents/Extensions/DateExtensions.cs
@@ -10,10 +10,7 @@ namespace BlazorComponents.Extensions
             try
             {
                 DateTime dt = DateTime.Parse(strDate);
-                if ((dt.Month != System.DateTime.Now.Month) || (dt.Day < 1 && dt.Day > 31) || dt.Year != System.DateTime.Now.Year)
-                    return false;
-                else
-                    return true;
+                return true;
             }
             catch
             {
@@ -23,18 +20,15 @@ namespace BlazorComponents.Extensions
 
         public static DateTime ToDate(this object obj)
         {
-            if (obj.IsDate())
+            try
             {
                 string strDate = obj.ToString();
-                try
-                {
-                    DateTime dt = DateTime.Parse(strDate);
-                }
-                catch
-                {
-                }
+                return DateTime.Parse(strDate);
             }
-            return DateTime.Now;
+            catch
+            {
+                return DateTime.Now;
+            }
         }
     }
 }
