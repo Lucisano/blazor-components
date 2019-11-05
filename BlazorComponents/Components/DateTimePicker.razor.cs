c19d0c444b293a579141ff162adeee99d2d28c68
antony lucisano <antony.lucisano@ukfast.co.uk>
2019-11-05 08:50:24 +0000

    Initial Commit

---
+++ BlazorComponents/Components/DateTimePicker.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorComponents.Components
+{
+    public class DateTimePickerBase : ComponentBase
+    {
+        #region Properties
+        private string _title { get; set; }
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                this.StateHasChanged();
+            }
+        }
+        #endregion
+    }
+}
