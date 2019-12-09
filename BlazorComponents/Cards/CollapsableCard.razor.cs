e307a483cedef879a707b31356aa505db93aab36
antony lucisano <antony.lucisano@ukfast.co.uk>
2019-12-09 12:15:04 +0000

    Cards added

---
+++ BlazorComponents/Cards/CollapsableCard.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponents.Cards
+{
+    public class CollapsableCardBase : ComponentBase
+    {
+
+        [Parameter]
+        public EventCallback<bool> ExpandChanged { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Header { get; set; }
+
+        [Parameter]
+        public bool Expanded { get; set; }
+
+        public string _expanded { get { return (Expanded) ? "true" : "false"; } }
+
+        public string HeaderClass { get { return (Expanded) ? "d-block card-header py-3" : "d-block card-header py-3 collapsed"; } }
+
+        public string ContentClass { get { return (Expanded) ? "collapse show" : "collapse"; } }
+
+        public void ToggleExpand()
+        {
+            Expanded = !Expanded;
+            ExpandChanged.InvokeAsync(Expanded);
+        }
+    }
+}
---
+++ BlazorComponents/Cards/DropDownCard.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorComponents.Cards
+{
+    public class DropDownCardBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        [Parameter]
+        public string Header { get; set; }
+
+        [Parameter]
+        public string DropDownHeader { get; set; }
+
+        [Parameter]
+        public List<Tuple<string, Action>> DropDownItems { get; set; }
+
+        [Parameter]
+        public bool Expanded { get; set; }
+
+        public string _expanded { get { return (Expanded) ? "true" : "false"; } }
+
+        public string DropDownDivClass { get { return (Expanded) ? "dropdown no-arrow show" : "dropdown no-arrow"; } }
+        public string DropDownMenuLinkClass { get { return (Expanded) ? "dropdown-menu dropdown-menu-right shadow animated--fade-in show" : "dropdown-menu dropdown-menu-right shadow animated--fade-in"; } }
+    }
+}
