fcad12ea1bacc97f429ee982aa59cac9fa96be7c
antony lucisano <antony.lucisano@ukfast.co.uk>
2020-01-06 10:02:02 +0000

    dropdown added

---
+++ BlazorComponents/Dropdown.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TestBlazorApp.Components
+{
+    public class DropdownBase : ComponentBase
+    {
+        [Parameter]
+        public string SelectedText { get; set; }
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
